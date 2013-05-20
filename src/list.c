ea946a7e91dd4640cb0a27956e78ddee8f374d83
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2013-05-20 12:39:05 +0800

    merged node.c to list.c

--- src/list.c
+++ src/list.c
@@ -3,7 +3,14 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "node.c"
+
+struct node {
+	void *value;
+	struct node *previous;
+	struct node *next;
+};
+
+typedef struct node node;
 
 struct list {
 	node *head;
@@ -14,6 +21,14 @@ struct list {
 typedef struct list list;
 
 /* ================================================================================
+ function prototypes for nodes
+ ================================================================================ */
+node *create_node(void *);
+void destroy_node(node *);
+void *get_value(node *);
+void set_value(node *, void *);
+
+/* ================================================================================
 	function prototypes for lists
 ================================================================================ */
 list *create_list(void);
@@ -22,9 +37,40 @@ void append(list *,void *);
 void *pop(list *);
 void *remove_first(list *);
 void destroy_list(list *);
-void insert_left(list *, node *ref,void *);
+void insert_left(list *, node *,void *);
 void delete_node(list *,node *);
 void merge_lists(list *, list *);
+
+/* ================================================================================
+ function definitions for nodes
+ ================================================================================ */
+node *create_node(void *value) {
+	node *n = (node *)malloc(sizeof(node));
+	
+	if (n == NULL) {
+		printf("MEMORY OVERFLOW!!!");
+		exit(1);
+	}
+	
+	n->value = value;
+	n->previous = NULL;
+	n->next = NULL;
+}
+
+void destroy_node(node *n) {
+	n->previous = NULL;
+	n->next = NULL;
+	n->value = NULL;
+	free(n);
+}
+
+void *get_value(node *n) {
+	return n->value;
+}
+
+void set_value(node *n, void *value) {
+	n->value = value;
+}
 
 /* ================================================================================
 	function definitions for lists
